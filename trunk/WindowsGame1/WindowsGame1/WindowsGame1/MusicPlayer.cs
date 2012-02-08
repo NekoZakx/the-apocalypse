b1f603f200514e699177a189f48de93c27f9545b
misterepic@gmail.com <misterepic@gmail.com@9e5d2aa7-b6ef-1a72-60ef-629148039a0e>
2012-02-08 17:45:22 +0000

    J'ai commencé la classe MusicPlayer.

---
+++ trunk/WindowsGame1/WindowsGame1/WindowsGame1/MusicPlayer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Media;
+
+namespace WindowsGame1
+{
+    class MusicPlayer
+    {
+        Song[] songs;
+
+        public MusicPlayer(Song[] songsArray)
+        {
+            songs = songsArray;
+        }
+
+        public void play(int songIndex)
+        {
+            MediaPlayer.Play(songs[songIndex]);
+        }
+
+        public void play(int songIndex, bool repeat)
+        {
+            MediaPlayer.IsRepeating = repeat;
+            MediaPlayer.Play(songs[songIndex]);
+        }
+
+        public void pause()
+        {
+            MediaPlayer.Pause();
+        }
+
+        public void stop()
+        {
+            MediaPlayer.Stop();
+        }
+
+        public void shuffle()
+        {
+            int loops = songs.Length * 2;
+            //Je suis rendu ici.
+        }
+    }
+}
