d7d203aee153ba92e86810478770374f8e28743a
misterepic@gmail.com <misterepic@gmail.com@9e5d2aa7-b6ef-1a72-60ef-629148039a0e>
2012-02-16 23:17:27 +0000

    Plein de changements du côté du son.
    Le principe du soundInstance a été abandonné. Grâce à ça, les sons peuvent être joués plusieurs fois sans qu'on aie à le loader plusieurs fois. Les options de volume et de pitch ont été conservés. Les options de Pan ont été ajoutés.

--- WindowsGame1/WindowsGame1/WindowsGame1/SFXBank.cs
+++ WindowsGame1/WindowsGame1/WindowsGame1/SFXBank.cs
@@ -8,25 +8,26 @@ namespace WindowsGame1
 {
     class SFXBank
     {
-        private SoundEffect[] sounds;
-        private SoundEffectInstance[] sfi;
+        private SoundEffect[] sounds;       
+        //private SoundEffectInstance[] sfi;
         private float volume = 1;
         private float pitch = 0;
+        private float pan = 0;
 
         public SFXBank(SoundEffect[] sounds)
         {
             this.sounds = sounds;
-            sfi = new SoundEffectInstance[this.sounds.Length];
+            /*sfi = new SoundEffectInstance[this.sounds.Length];
             for (int i = 0; i < sfi.Length; i++)
-                sfi[i] = sounds[i].CreateInstance();
+                sfi[i] = sounds[i].CreateInstance();*/
         }
 
         public void setPitch(float pitch)
         {
             if (pitch >= -1 && pitch <= 1)
             {
-                for (int i = 0; i < sfi.Length; i++)
-                    sfi[i].Pitch = pitch;
+                /*for (int i = 0; i < sfi.Length; i++)
+                    sfi[i].Pitch = pitch;*/
                 this.pitch = pitch;
             }
         }
@@ -40,8 +41,8 @@ namespace WindowsGame1
         {
             if (volume >= 0 && volume <= 1)
             {
-                for (int i = 0; i < sfi.Length; i++)
-                    sfi[i].Volume = volume;
+                /*for (int i = 0; i < sfi.Length; i++)
+                    sfi[i].Volume = volume;*/
                 this.volume = volume;
             }
         }
@@ -51,25 +52,41 @@ namespace WindowsGame1
             return this.volume;
         }
 
-        public SoundState getState(int soundIndex)
+        public void setPan(float pan)
+        {
+            if (pan >= -1 && pan <= 1)
+            {
+                /*for (int i = 0; i < sfi.Length; i++)
+                    sfi[i].pan = pan;*/
+                this.pan = pan;
+            }
+        }
+
+        public float getPan()
+        {
+            return this.pan;
+        }
+
+        /*public SoundState getState(int soundIndex)
         {
             return sfi[soundIndex].State;
-        }
+        }*/
 
         public void play(int soundIndex)
         {
-            sfi[soundIndex].Play();
+            //sfi[soundIndex].Play();
+            sounds[soundIndex].Play(this.volume, this.pitch, this.pan);
         }
 
-        public void stop(int soundIndex)
+        /*public void stop(int soundIndex)
         {
             sfi[soundIndex].Stop();
-        }
+        }*/
 
-        public void stopAll()
+        /*public void stopAll()
         {
             for (int i = 0; i < sfi.Length; i++)
                 sfi[i].Stop();
-        }
+        }*/
     }
 }
