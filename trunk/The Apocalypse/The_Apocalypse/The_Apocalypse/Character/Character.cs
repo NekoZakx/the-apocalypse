68958fb0b3edb72306790a22ec8bc781a96e8ede
misterepic@gmail.com <misterepic@gmail.com@9e5d2aa7-b6ef-1a72-60ef-629148039a0e>
2012-03-03 20:10:06 +0000

    Avancement dans les classes Character, Player, Position et Weapon.

--- trunk/The Apocalypse/The_Apocalypse/The_Apocalypse/Character/Character.cs
+++ trunk/The Apocalypse/The_Apocalypse/The_Apocalypse/Character/Character.cs
@@ -2,10 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 
 namespace The_Apocalypse
 {
     interface Character
     {
+        string name
+        {
+            get;
+            set;
+        }
+        int hp
+        {
+            get;
+            set;
+        }
+        Position position
+        {
+            get;
+            set;
+        }
+
+        void Update(GameTime gameTime);
+
+        void Draw(GameTime gameTime);
+
+        void LoadContent(ContentManager contentManager);
     }
 }
