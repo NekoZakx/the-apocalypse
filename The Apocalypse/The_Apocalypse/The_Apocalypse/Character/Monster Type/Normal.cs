d83dd57a554fe5e9f634adaabf92caa2fc4646cc
misterepic@gmail.com <misterepic@gmail.com@9e5d2aa7-b6ef-1a72-60ef-629148039a0e>
2012-03-03 20:48:43 +0000

    Update Monster.cs et Normal.cs

---
+++ The Apocalypse/The_Apocalypse/The_Apocalypse/Character/Monster Type/Normal.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace The_Apocalypse
+{
+    class Normal : Monster
+    {
+
+        private string _name = "Normal";
+        private int _damage = 1;
+        private int _hp = 20;
+        private Area _area; //À faire (Zone::Near)
+
+        public Normal()
+        {
+        }
+
+        public int damage
+        {
+            get
+            {
+                return _damage;
+            }
+            set
+            {
+                _damage = value;
+            }
+        }
+
+        public Area area
+        {
+            get
+            {
+                return _area;
+            }
+            set
+            {
+                _area = value;
+            }
+        }
+
+        public void attack()
+        {
+            //À faire
+        }
+
+        public void move()
+        {
+            //À faire
+        }
+    }
+}
