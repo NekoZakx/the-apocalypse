93ad73e596102bea3ea7772b10cf699c588d5aa0
misterepic@gmail.com <misterepic@gmail.com@9e5d2aa7-b6ef-1a72-60ef-629148039a0e>
2012-03-03 20:48:43 +0000

    Update Monster.cs et Normal.cs

--- trunk/The Apocalypse/The_Apocalypse/The_Apocalypse/Character/Monster.cs
+++ trunk/The Apocalypse/The_Apocalypse/The_Apocalypse/Character/Monster.cs
@@ -2,10 +2,78 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 
 namespace The_Apocalypse
 {
-    class Monster
+    class Monster : Character
     {
+        private string _name;
+        private int _hp;
+        private Position _position;
+        
+        int damage
+        {
+            get;
+            set;
+        }
+
+        Area area
+        {
+            get;
+            set;
+        }
+
+        public string name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public int hp
+        {
+            get
+            {
+                return _hp;
+            }
+            set
+            {
+                _hp = value;
+            }
+        }
+
+        public Position position
+        {
+            get
+            {
+                return _position;
+            }
+            set
+            {
+                _position = value;
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            //À faire
+        }
+
+        public void Draw(GameTime gameTime)
+        {
+            //À faire
+        }
+
+        public void LoadContent(ContentManager contentManager)
+        {
+            //À faire
+        }
     }
 }
