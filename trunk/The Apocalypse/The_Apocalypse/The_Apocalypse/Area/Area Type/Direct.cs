9f1da92a6ac0d8e48d3569984121681d83232700
samuel_grenon@msn.com <samuel_grenon@msn.com@9e5d2aa7-b6ef-1a72-60ef-629148039a0e>
2012-03-04 19:51:48 +0000

    J'ai ajouté la classe Direct qui permet de tirer une balle en ligne droite.

---
+++ trunk/The Apocalypse/The_Apocalypse/The_Apocalypse/Area/Area Type/Direct.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace The_Apocalypse
+{
+    class Direct : Area
+    {
+        int M = 0, B = 0, Orientation = 0;
+        Position positionNow = new Position(0, 0);
+
+        public void Direct(int x1, int x2, int y1, int y2)
+        {
+
+            if (x2 - x1 < 0)
+            {
+                Orientation = -1;
+            }
+            else
+            {
+                Orientation = 1;
+            }
+
+            M = ((y2 - y1) / (x2 - x1));
+            B = y1 - (M * x1);
+
+        }
+
+        public void UpdatePosition()
+        {
+            positionNow.X = (int)(positionNow.X + (Orientation) * (1 / (Math.Pow(2, Math.Abs(M)))));
+            positionNow.Y = (M * positionNow.X) + B;
+        }
+    }
+}
