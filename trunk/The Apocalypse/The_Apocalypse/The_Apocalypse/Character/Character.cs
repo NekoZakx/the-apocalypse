280e806757c2a536f369d5dfb3194b98bbc39795
Zack.Niwa@gmail.com <Zack.Niwa@gmail.com@9e5d2aa7-b6ef-1a72-60ef-629148039a0e>
2012-03-04 20:25:36 +0000

    Mise à jour Character

--- trunk/The Apocalypse/The_Apocalypse/The_Apocalypse/Character/Character.cs
+++ trunk/The Apocalypse/The_Apocalypse/The_Apocalypse/Character/Character.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace The_Apocalypse
 {
@@ -30,11 +31,11 @@ namespace The_Apocalypse
             set;
         }
 
-        void move(float angle);
+        void move();
 
         void Update(GameTime gameTime);
 
-        void Draw(GameTime gameTime);
+        void Draw(SpriteBatch srpiteBatch);
 
         void LoadContent(ContentManager contentManager);
     }
