61e5878151ff6d3f4e27f80eb539cc89f611acf6
samuel_grenon@msn.com <samuel_grenon@msn.com@9e5d2aa7-b6ef-1a72-60ef-629148039a0e>
2012-03-24 18:46:39 +0000

    J'ai ajouté les méthodes à faire dans Area.

--- trunk/The Apocalypse/The_Apocalypse/The_Apocalypse/Area/Area.cs
+++ trunk/The Apocalypse/The_Apocalypse/The_Apocalypse/Area/Area.cs
@@ -7,5 +7,20 @@ namespace The_Apocalypse
 {
     interface Area
     {
+        Position positionStart;
+        Position positionEnd;
+        int orientation;
+        Position positionNow;
+
+        public Position getPosition()
+        {
+            //À faire
+            return positionNow;
+        }
+
+        public void UpdatePosition()
+        {
+            //À faire
+        }
     }
 }
