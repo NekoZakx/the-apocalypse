76a324ff0934c9743210bb9fe04d8987018fb0a4
samuel_grenon@msn.com <samuel_grenon@msn.com@9e5d2aa7-b6ef-1a72-60ef-629148039a0e>
2012-04-09 19:53:39 +0000

    Ajout de la classe Proximity dans le dossier Area et ajout de la classe ChainSaw dans le dossier Weapons

---
+++ The Apocalypse/The_Apocalypse/The_Apocalypse/Area/Area Type/Proximity.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System.Threading;
+
+namespace The_Apocalypse
+{
+    class Proximity : Area
+    {
+        double deltaX = 0, deltaY = 0;
+        double Mper = 0, Yper = 0, Xper = 0, Y = 0, X = 0, slope = 0, Bper = 0;
+        bool underRight = true, upperRight = true, leftOfRight = true, rightOfRight = true;
+        bool hit = false;
+        bool multikill = false;
+        public bool state = false;
+
+        Position positionNow = new Position(0, 0);
+        Position positionStart = new Position(0, 0);
+        Position positionEnd = new Position(0, 0);
+
+        public Proximity(Position player, Position mouse)
+        {
+            positionStart.X = player.X;
+            positionStart.Y = player.Y;
+            positionNow.X = positionStart.X;
+            positionNow.Y = positionStart.Y;
+        }
+
+        public bool proximity(Position player, Position normal, Position mouse, GraphicsDevice GraphicsDevice)
+        {
+            if (Math.Pow((normal.X - player.X), 2) + Math.Pow((normal.Y + player.Y), 2) <= 100)//Si le point se trouve dans le cercle on retourne vraie
+            {
+                deltaX = mouse.X - player.X;
+                deltaY = mouse.Y - player.Y;
+
+                slope = deltaY / deltaX;//On calcule la pente de la première droite
+
+                if (slope == 0)
+                {
+                    Mper = double.PositiveInfinity;//On calcule la pente de la droite perpendiculaire
+                }
+                else
+                {
+                    Mper = -1 / slope;
+                }
+
+                if (Mper == 0)
+                {
+                    Bper = player.Y;
+                }
+                else
+                {
+                    Bper = player.Y / (Mper * player.X);//On calcule le B de la droite perpendiculaire
+                }
+
+                Yper = (Mper * player.X) + Bper;//On calcule la formule de la droite perpendiculaire
+
+                Y = (Mper * normal.X) + Bper;//On trouve la valeur du Y pour le X de notre point sur la droite
+
+                if (Mper == 0)
+                {
+                    X = Bper;
+                }
+                else
+                {
+                    X = (Y / Mper) + Bper;//On trouve la valeur du X pour le Y de notre point sur la droite
+                }
+
+                if (mouse.Y < Y)//Si le point à évalué est plus petit que le point trouvé, le point se trouve en dessous de la droite
+                {
+                    return underRight;
+                }
+                else if (mouse.Y > Y)//Sinon le point se trouve au dessus
+                {
+                    return upperRight;
+                }
+
+                if (mouse.X < X)//Si le point à évalué est plus petit que le point trouvé, le point se trouve à gauche de la droite
+                {
+                    return leftOfRight;  
+                }
+                else//Sinon il se trouve à droite
+                {
+                    return rightOfRight;
+                }
+            }
+
+            return false;
+        }
+
+        public bool isInCircle(Position player, Position normal)
+        {
+            if (Math.Pow((normal.X - player.X), 2) + Math.Pow((normal.Y + player.Y), 2) <= 100)//Si le point se trouve dans le cercle on retourne vraie
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool CompareAreatoLine(Position point1, Position point2)
+        {
+            if (!hit || multikill)
+                if (positionNow.X > point1.X && positionNow.X < point2.X)
+                {
+                    if (positionNow.Y > point1.Y && positionNow.Y < point2.Y)
+                    {
+                        hit = true;
+                    }
+                }
+            if (!state && hit)
+            {
+                if (multikill)
+                    hit = false;
+                else
+                {
+                    state = true;
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ The Apocalypse/The_Apocalypse/The_Apocalypse/Weapons/Weapon Type/ChainSaw.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+using System.Threading;
+
+namespace The_Apocalypse.Weapons.Weapon_Type
+{
+    class ChainSaw : Weapon
+    {
+        private string _name = "ChainSaw";
+        private int _damage = 30;
+        private int _ammo = -1;
+        private List<Proximity> near;
+
+        private float _speed = 0;
+
+        public string name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public int damage
+        {
+            get
+            {
+                return _damage;
+            }
+            set
+            {
+                _damage = value;
+            }
+        }
+
+        public int ammo
+        {
+            get
+            {
+                return _ammo;
+            }
+            set
+            {
+                _ammo = value;
+            }
+        }
+
+        public float speed
+        {
+            get
+            {
+                return _speed;
+            }
+            set
+            {
+                _speed = value;
+            }
+        }
+
+        public int hit(Position point1, Position point2)
+        {
+            foreach (Proximity point in near)
+            {
+                if (point.CompareAreatoLine(point1, point2))
+                {
+                    return _damage;
+                }
+            }
+            return 0;
+        }
+    }
+}
